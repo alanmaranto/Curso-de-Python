8f1dc479ea646497eb8bb0a55ad314b3cdf703a1
alanmaranto <alanarostegui@hotmail.com>
2019-12-14 13:03:11 -0600

    primera Clase

---
+++ primeraClase.py
@@ -0,0 +1,15 @@
+import turtle
+
+window = turtle.Screen()
+alan = turtle.Turtle()
+alan.forward(50)
+alan.left(90)
+alan.forward(50)
+alan.left(90)
+alan.forward(50)
+alan.left(90)
+alan.forward(50)
+alan.left(90)
+
+# turtle no nos cierre la venta
+turtle.mainloop()
